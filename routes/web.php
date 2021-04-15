dac6da07bc97700c7333dc8671e448e5e9ea67b2
monarchclk <995958406@qq.com>
2021-04-15 18:30:56 +0800

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 });*/
 Route::get('/','PagesController@root')->name('root');
 Route::resource('users','UsersController',['only'=> ['show','update','edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
